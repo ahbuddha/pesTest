81a087fbd1d300fe1d1b88d9e18dfa022731e953
AHolcomb <a.holcomb@avanade.com>
2018-10-02 14:02:34 -0700

    changed message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'pesTest' });
+  let model = { 
+    title: 'pesTest',
+    msg: process.env.MSG || 'This is dev'
+  };
+  res.render('index', model);
 });
 
 module.exports = router;
